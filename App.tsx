22d88199d0d1a63cd0050bd4539aeb7c016364eb
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-02 21:23:04 -0300

    feat: Fonte da aplicação

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+      {fontsLoaded ? <Text>Ignite Gym</Text> : <View />}
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
