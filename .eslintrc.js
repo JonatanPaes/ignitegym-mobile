169a6191e40087ad596b641a4f6fca558ff4880d
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-02 21:11:50 -0300

    setup prettier e eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^react', '^@?\\w'],
+
+          ['^@screens'],
+          ['^@components'],
+          ['^@hooks'],
+          ['^@store'],
+          ['^@storage'],
+          ['^@routes'],
+          ['^@utils'],
+
+          ['^\\.+'],
+
+          [
+            '^.+\\.css$',
+            '^.+\\.(s(c|a)ss)$',
+            '^.+\\.less$',
+            '^.+\\.styl$',
+            '^.+\\.theme$'
+          ]
+        ]
+      }
+    ],
+    'prettier/prettier': 'error'
+  }
+}
