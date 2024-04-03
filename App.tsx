ef2d0e2fb6978780a017e206082254a9e8efc36f
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-02 21:26:05 -0300

    feat: Personalizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 import {
   Roboto_400Regular,
   Roboto_700Bold,
@@ -12,7 +12,19 @@ export default function App() {
   })
 
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+    <View
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#202024'
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Ignite Gym</Text> : <View />}
     </View>
   )
