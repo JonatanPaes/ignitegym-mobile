43fde02c4aa92d2035722ea4fb40be2c434a18cb
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-03 21:52:19 -0300

    chore: Customizando o tema default

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   useFonts
 } from '@expo-google-fonts/roboto'
 import { NativeBaseProvider } from 'native-base'
+import { THEME } from 'src/theme'
 
 import { Loading } from '@components/Loading'
 
@@ -15,14 +16,13 @@ export default function App() {
   })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {/* {fontsLoaded ? <View /> : <Loading />} */}
-      <Loading />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   )
 }
