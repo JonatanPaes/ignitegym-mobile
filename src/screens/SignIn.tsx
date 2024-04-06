b63504a1f63ae5216c0ac2f1d1b86508496338c1
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-05 22:29:30 -0300

    feat: Componente de Input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,6 +2,8 @@ import { Center, Heading, Image, Text, VStack } from 'native-base'
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 
+import { Input } from '@components/Input'
+
 export function SignIn() {
   return (
     <VStack flex={1} bg="gray.700">
@@ -24,6 +26,9 @@ export function SignIn() {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse sua conta
         </Heading>
+
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
       </Center>
     </VStack>
   )
