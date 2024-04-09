5186e081a21a51290e2a43fffa96ead3478ed0a7
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-08 21:54:10 -0300

    feat: Criando a SignUp

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from '@expo-google-fonts/roboto'
 import { THEME } from 'src/theme'
 
-import { SignIn } from '@screens/SignIn'
+import { SignUp } from '@screens/SignUp'
 
 import { Loading } from '@components/Loading'
 
@@ -24,7 +24,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   )
 }
