c9a23d47ba3d283eec89d21926e5e572f80f20b9
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-13 16:01:11 -0300

    style: Estilizando o HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,11 +2,15 @@ import { Heading, HStack, Text, VStack } from 'native-base'
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">Olá,</Text>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
 
-        <Heading color="gray.100">Jonatán</Heading>
+        <Heading color="gray.100" fontSize="md">
+          Jonatán
+        </Heading>
       </VStack>
     </HStack>
   )
