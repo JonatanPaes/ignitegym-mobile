f88820a44b7cfbdba313b20e4e366345c75aa7ee
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-16 22:51:48 -0300

    feat: Inputs de senha

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 import { TouchableOpacity } from 'react-native'
-import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base'
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack
+} from 'native-base'
 
+import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
@@ -49,6 +57,24 @@ export function Profile() {
 
           <Input bg="gray.600" placeholder="E-mail" isDisabled />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mt={4} />
+        </VStack>
       </ScrollView>
     </VStack>
   )
