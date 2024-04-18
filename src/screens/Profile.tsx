43c4c9fbb372d16d50bb9581d12ddd420ca74af4
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-17 21:54:02 -0300

    refactor:

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -23,7 +23,7 @@ export function Profile() {
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -58,8 +58,14 @@ export function Profile() {
           <Input bg="gray.600" placeholder="E-mail" isDisabled />
         </Center>
 
-        <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+        <Center px={10} mt={12} mb={9}>
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+          >
             Alterar senha
           </Heading>
 
@@ -74,7 +80,7 @@ export function Profile() {
           />
 
           <Button title="Atualizar" mt={4} />
-        </VStack>
+        </Center>
       </ScrollView>
     </VStack>
   )
