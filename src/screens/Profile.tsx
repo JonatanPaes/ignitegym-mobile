0eb8ef0a925f99bb4ff94c77c71427f92c428d9a
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-04-22 20:38:46 -0300

    feat: Exibindo a foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -19,9 +19,12 @@ const PHOTO_SIZE = 33
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState(
+    'https://github.com/jonatanpaes.png'
+  )
 
   async function handleUserPhotoSelect() {
-    const photoSelected = await ImagePicker.launchCameraAsync({
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
       aspect: [4, 4],
@@ -31,6 +34,8 @@ export function Profile() {
     if (photoSelected.canceled) {
       return
     }
+
+    setUserPhoto(photoSelected.assets[0].uri)
   }
 
   return (
@@ -49,7 +54,7 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: 'https://github.com/jonatanpaes.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
