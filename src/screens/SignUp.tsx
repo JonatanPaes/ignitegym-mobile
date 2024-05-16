a5e9fea46d20d94c0b6f1bee11e141cffac8adcf
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-05-15 21:42:41 -0300

    feat: Criando conta e logando usuario

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import {
   Center,
@@ -15,6 +16,8 @@ import * as yup from 'yup'
 
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
+
+import { useAuth } from '@hooks/useAuth'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
@@ -44,6 +47,8 @@ const signUpSchema = yup.object({
 type FormData = yup.InferType<typeof signUpSchema>
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false)
+
   const {
     control,
     handleSubmit,
@@ -55,6 +60,7 @@ export function SignUp() {
   const { goBack } = useNavigation()
 
   const toast = useToast()
+  const { signIn } = useAuth()
 
   function handleGoBack() {
     goBack()
@@ -64,14 +70,16 @@ export function SignUp() {
     const { name, email, password } = data
 
     try {
-      const response = await api.post('/users', {
+      setIsLoading(true)
+      await api.post('/users', {
         name,
         email,
         password
       })
 
-      console.log(response.data)
+      await signIn(email, password)
     } catch (error) {
+      setIsLoading(false)
       const isAppError = error instanceof AppError
 
       const title = isAppError
@@ -173,6 +181,7 @@ export function SignUp() {
           <Button
             title="Criar e acessar"
             onPress={handleSubmit(handleSignUp)}
+            isLoading={isLoading}
           />
         </Center>
 
