aa81f563e924c071e925e965ae96a8528ea7f3e0
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-05-15 21:29:20 -0300

    feat: Exibindo a foto padrão do usuário

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -4,6 +4,8 @@ import { MaterialIcons } from '@expo/vector-icons'
 
 import { useAuth } from '@hooks/useAuth'
 
+import defaultUserPhotoImg from '@assets/userPhotoDefault.png'
+
 import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
@@ -12,7 +14,7 @@ export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
-        source={{ uri: 'https://github.com/jonatanpaes.png' }}
+        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
         size={16}
         alt="Imagem do usuário"
         mr={4}
