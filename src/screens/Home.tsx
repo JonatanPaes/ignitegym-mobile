1c5de32a215be6d4ef6fda42ebedf156a2ace844
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-05-21 19:18:23 -0300

    feat: Criando o ExerciseDTO

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from 'react'
 import { FlatList, Heading, HStack, Text, useToast, VStack } from 'native-base'
+import { ExerciseDTO } from '@dtos/ExerciseDTO'
 import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { api } from '@services/api'
 
@@ -14,7 +15,7 @@ import { AppError } from '@utils/AppError'
 export function Home() {
   const [groupSelected, setGroupSelected] = useState('Costas')
   const [groups, setGroups] = useState<string[]>([])
-  const [exercises, setExercises] = useState([])
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([])
 
   const toast = useToast()
   const { navigate } = useNavigation<AppNavigatorRoutesProps>()
@@ -44,7 +45,7 @@ export function Home() {
   async function fetchExercisesByGroup() {
     try {
       const response = await api.get(`/exercises/bygroup/${groupSelected}`)
-      console.log(response.data)
+      setExercises(response.data)
     } catch (error) {
       const isAppError = error instanceof AppError
       const title = isAppError
@@ -108,7 +109,7 @@ export function Home() {
 
         <FlatList
           data={exercises}
-          keyExtractor={(item) => item}
+          keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <ExerciseCard onPress={handleOpenExerciseDetails} />
           )}
