6aeec123d3f3545c79feed012430d57be643a6bb
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-05-21 19:13:30 -0300

    feat: Buscando Exercícios por grupo

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { FlatList, Heading, HStack, Text, useToast, VStack } from 'native-base'
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { api } from '@services/api'
 
 import { ExerciseCard } from '@components/ExerciseCard'
@@ -14,12 +14,7 @@ import { AppError } from '@utils/AppError'
 export function Home() {
   const [groupSelected, setGroupSelected] = useState('Costas')
   const [groups, setGroups] = useState<string[]>([])
-  const [exercises, setExercises] = useState([
-    'Puxada frontal',
-    'Remada curvada',
-    'Remada unilateral',
-    'Levantamento terras'
-  ])
+  const [exercises, setExercises] = useState([])
 
   const toast = useToast()
   const { navigate } = useNavigation<AppNavigatorRoutesProps>()
@@ -46,9 +41,33 @@ export function Home() {
     }
   }
 
+  async function fetchExercisesByGroup() {
+    try {
+      const response = await api.get(`/exercises/bygroup/${groupSelected}`)
+      console.log(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar os exercícios'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
+  }
+
   useEffect(() => {
     fetchGroups()
   }, [])
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchExercisesByGroup()
+    }, [groupSelected])
+  )
 
   return (
     <VStack flex={1}>
